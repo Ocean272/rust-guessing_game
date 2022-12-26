be20503461e1c97babac1490f8b8ba310a323c46
ocean272la <ocean272la@penguin>
2022-12-26 21:23:44 +0800

    update comment

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,22 @@ fn main() {
 
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read line");
+            .expect("Failed to read line"); // Rust will insist coder to write "error-handling" code as its in io standard input.
 
         
         // Shadowing allow reuse the 'guess' variable.    
-        let guess: u32 = match guess.trim().parse(){
-            //.expect("Please type a number")
-            Ok(num) => num,
+        let guess: u32 = match guess.trim().parse(){ //here it switch from expect call to match expression to move from
+            //.expect("Please type a number")          crashing on an error to handling the error.
+            Ok(num) => num, 
+            // if parse is able to successfully turn the string into a number, it will return an Ok value
+            // hence, match the first arm's pattern and the match expression will just return the num value.
+            // The value will then end up right where we want it in the new guess variable we're creating. 
             Err(_) => continue,
+            // if parse is not able to turn the string into a number, it will return an Err value.  Since the 
+            // Err value does not match the Ok(num) pattern in the first match arm, it does match the Err(_) pattern
+            // in the second arm, The underscore is a catchall value. So if the program execute the second arm's code 'continue',
+            // the program will go into the next iteration of the loop and ask for another guess. Therefore,
+            // effectively, the program ignores all errors that parse might encounter.
         };
 
 
